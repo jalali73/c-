22b8369bada9c2e7fd57e163f825cd5f32e4c432
jalali <jrdsaeed@gmail.com>
2017-03-05 22:20:42 -0800

    second commited

---
+++ program/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static int[] mod = new int[8];
+        static void convert(int number)
+        {
+            int i = 0;
+            while (number != 0)
+            {
+                mod[i] = number % 2;
+                number = number / 2;
+                i++;
+            }
+            for (int j = 7; j >= 0; j--)
+            {
+                Console.Write(mod[j]);
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int div;
+            div = int.Parse(Console.ReadLine());
+            convert(div);
+        }
+
+
+    }
+}
+
+
